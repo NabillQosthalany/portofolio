70d3afe0e293080422f7d29cf4a35651d3986c38
Nabil Qosthalany <nabilzqosthalany.com>
2023-11-18 08:26:31 +0700

    first commit

--- src/components/Portofolio/portofolio.vue
+++ src/components/Portofolio/portofolio.vue
@@ -98,6 +98,7 @@ export default {
         <img
           v-show="item.isHover || item.isButtonHover"
           class="z-50 absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          src="/images/githublogo.webp"
           alt=""
         />
         <button
